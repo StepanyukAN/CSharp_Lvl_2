4a9ede7b82f2bb40fd3b4e0ff3dc7edb49880dba
StepanyukAN <fargogi@mail.ru>
2018-10-18 21:05:34 +1000

    ДЗ-5

---
+++ Lesson5/Lesson5/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    /// <summary>
+    /// Отдел
+    /// </summary>
+    class Department
+    {
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get;}
+        public List<Employee> Employees { get => employees; set => employees = value; }
+
+        /// <summary>
+        /// Список сотрудников
+        /// </summary>
+        private List<Employee> employees;
+
+        public Department(string name) { Name = name; Employees = new List<Employee>(); }
+
+    }
+}
---
+++ Lesson5/Lesson5/WorkerEditor.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lesson5
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkerEditor.xaml
+    /// </summary>
+    public partial class WorkerEditor : Window
+    {
+        public WorkerEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddWorker();
+        }
+
+        /// <summary>
+        /// Добавляем работника в отдел
+        /// </summary>
+        private void AddWorker()
+        {
+            MainWindow.Departments[cmbDept.SelectedIndex].Employees.Add(
+                            new Employee(txtName.Text, DTPBirthday.DisplayDate, txtPosition.Text));
+            MainWindow.Save();
+            this.Hide();
+        }
+    }
+}
